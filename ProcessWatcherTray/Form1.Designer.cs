8dcf56bcc8b571eb6c7bdff156bbd04286035a54
Bill Jackson <williamrjackson@gmail.com>
2023-01-31 00:17:41 -0500

    initial commit (from work years ago)

---
+++ ProcessWatcherTray/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ProcessWatcherTray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Process Watcher";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "exe";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(550, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(550, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(12, 15);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(531, 144);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(550, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Scan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 168);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Process Watcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ProcessWatcherTray/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace ProcessWatcherTray
+{
+    public partial class Form1 : Form
+    {
+        private static System.Timers.Timer timer;
+        private static List<string> processPaths = new List<string>();
+        private static string persistPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "processPaths.txt");
+        private static string initBrowseDir = AppDomain.CurrentDomain.BaseDirectory;
+        string persistFileText = string.Empty;
+
+        private HashSet<string> runningProcs = new HashSet<string>();
+        private bool AllRunning => runningProcs.Count > 0 && runningProcs.Count == processPaths.Count;
+
+        private bool IsScanning => timer != null && timer.Enabled;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PathsDataChanged()
+        {
+            listBox1.Items.Clear();
+            persistFileText = string.Empty;
+            
+            if (processPaths.Count > 0)
+            {
+                for (int i = 0; i < processPaths.Count; i++)
+                {
+                    listBox1.Items.Add(processPaths[i]);
+                    persistFileText += $"{processPaths[i]}{Environment.NewLine}";
+                }
+                File.WriteAllText(persistPath, persistFileText.Trim(Environment.NewLine.ToCharArray()));
+                StartTimer();
+            }
+            else
+            {
+                StopTimer();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(persistPath))
+            {
+                persistFileText = File.ReadAllText(persistPath);
+                processPaths = new List<string>(persistFileText.Trim(Environment.NewLine.ToCharArray()).Split(Environment.NewLine.ToCharArray()));
+                processPaths.RemoveAll((s) => string.IsNullOrWhiteSpace(s) || !File.Exists(s));
+                PathsDataChanged();
+            }
+        }
+
+        private void MinimizeToTray()
+        {
+            if (!this.IsHandleCreated) CreateHandle();
+            this.Invoke(new MethodInvoker(delegate () {
+                Hide();
+                WindowState = FormWindowState.Minimized;
+                notifyIcon.Visible = true;
+            }));
+        }
+
+        private void OpenFromTray()
+        {
+            if (!this.IsHandleCreated) CreateHandle();
+            this.Invoke(new MethodInvoker(delegate () {
+                Show();
+                this.WindowState = FormWindowState.Normal;
+                notifyIcon.Visible = false;
+            }));
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                StartTimer();
+                MinimizeToTray();
+            }
+        }
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            OpenFromTray();
+            StopTimer();
+        }
+
+        private static bool ProcessIsRunning(string FullPath)
+        {
+            string FilePath = Path.GetDirectoryName(FullPath);
+            string FileName = Path.GetFileNameWithoutExtension(FullPath).ToLower();
+            bool isRunning = false;
+
+            Process[] pList = Process.GetProcessesByName(FileName);
+
+            foreach (Process p in pList)
+            {
+                if (p.MainModule.FileName.StartsWith(FilePath, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    isRunning = true;
+                    break;
+                }
+            }
+            return isRunning;
+        }
+
+        private void StartTimer()
+        {
+            if (timer == null || !timer.Enabled)
+            {
+                //Console.WriteLine("Timer Started");
+                button3.Text = "Stop";
+                timer = new System.Timers.Timer(5000);
+                timer.Elapsed += OnTimedEvent;
+                timer.AutoReset = true;
+                timer.Enabled = true;
+            }
+        }
+        private void StopTimer()
+        {
+            if (timer != null && timer.Enabled)
+            {
+                //Console.WriteLine("Timer Stopped");
+                button3.Text = "Scan";
+                timer.Enabled = false;
+            }
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            //Console.WriteLine("Timer!");
+            foreach (var path in processPaths)
+            {
+                bool isRunning = ProcessIsRunning(path);
+                if (!isRunning)
+                {
+                    runningProcs.Remove(path);
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = path
+                    };
+                    Process.Start(startInfo);
+                }
+                else
+                {
+                    runningProcs.Add(path);
+                }
+            }
+
+            if (AllRunning)
+            {
+                MinimizeToTray();
+            }
+            else
+            {
+                OpenFromTray();
+            }
+        }
+        private void PersistPaths()
+        {
+            persistFileText = string.Empty;
+            for (int i = 0; i < processPaths.Count; i++)
+            {
+                string lineEnd = (i == processPaths.Count - 1) ? "\n" : "";
+                persistFileText += $"{processPaths[i]}{lineEnd}";
+            }
+            File.WriteAllText(persistPath, persistFileText);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = initBrowseDir;
+            openFileDialog1.Filter = "exe files (*.exe)|*.exe";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                initBrowseDir = Path.GetDirectoryName(openFileDialog1.FileName);
+                AddExePath(openFileDialog1.FileName);
+            }
+        }
+        private void AddExePath(string path)
+        {
+            if (path.EndsWith(".exe") && File.Exists(path))
+            {
+                if (!processPaths.Contains(path, StringComparer.InvariantCultureIgnoreCase))
+                {
+                    processPaths.Add(path);
+                    PathsDataChanged();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (IsScanning)
+            {
+                StopTimer();
+            }
+            else
+            {
+                StartTimer();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null) return;
+            string toRemove = (string)listBox1.SelectedItem;
+            if (processPaths.Remove(toRemove))
+            {
+                runningProcs.Remove(toRemove);
+                PathsDataChanged();
+            }
+        }
+    }
+}
